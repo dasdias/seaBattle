b18c437e02a28dbdbcbe0e1d50aad6d89a50536a
Alexander Dianov <dasdias@yandex.ru>
2020-03-06 20:06:20 +0300

    sea battle day - 3

--- js/script.js
+++ js/script.js
@@ -9,25 +9,86 @@ const again = document.getElementById('again');
 const header = document.querySelector('.header');
 
 const game = { // В объект game записано количество кораблей и их позиции в виде номеров id 
-    ships: [
-        {
-            location: ['26','36', '46', '56'],
-            hit: ['','','','']
-        },
-        {
-            location: ['11','12','13'],
-            hit: ['','','']
-        },
-        {
-            location: ['69', '79'],
-            hit: ['','']
-        },
-        {
-            location: ['32'],
-            hit: ['']
-        },
-    ],
-    shipCount: 4,
+    ships: [],
+    shipCount: 0,
+    optionShip: {
+        count: [1, 2, 3, 4],
+        size: [4, 3, 2, 1]
+    },
+    collision: new Set(),
+    generateShip() {
+        for (let i = 0; i < this.optionShip.count.length; i++) {
+            // console.log(this.optionShip.count[i]);
+            for (let j = 0; j < this.optionShip.count[i]; j++) {
+                // console.log(this.optionShip.size[i]);
+                const size = this.optionShip.size[i];
+                const ship = this.generateOptionsShip(size);
+                this.ships.push(ship);
+                this.shipCount++;
+            }
+        }
+    },
+    generateOptionsShip(shipSize) {
+        const ship = {
+            hit: [],
+            location: [],
+        };
+
+        const direction = Math.random() < 0.5; // получаем рандомное true или false
+        let x, y;
+
+        if(direction) { // располагаем корабль, горизонтально или вертикально
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * (10 - shipSize));
+        } else {
+            x = Math.floor(Math.random() * (10 - shipSize));
+            y = Math.floor(Math.random() * 10);
+        }
+
+        for (let i = 0; i < shipSize; i++) {
+           if(direction) {
+                ship.location.push(x + '' + (y + i))
+            } else {
+                ship.location.push((x + i) + '' + y)                
+           }
+           ship.hit.push('');
+        }
+
+        if (this.checkCollision(ship.location)) {
+            return this.generateOptionsShip(shipSize);
+        }
+
+        this.addCollision(ship.location);
+
+        return ship;
+    },
+    checkCollision(location) {
+        for (const coord of location) {
+            if(this.collision.has(coord)) {
+                return true;
+            }
+        }
+    },
+    addCollision(location) {
+        for (let i = 0; i < location.length; i++) {
+            const startCoordX =location[i][0] - 1;
+
+            for (let j = startCoordX; j < startCoordX + 3; j++) {
+                const startCoordY = location[i][1] -1;
+
+                for (let z = startCoordY; z < startCoordY + 3; z++) {
+
+                    if (j >= 0 && j < 10 && z >= 0 && z < 10 ) {
+                        const coord = j + '' + z;
+                        
+                            this.collision.add(coord);
+                      
+                    }
+
+                }
+            }
+        }
+    }
 };
 
 const play = {
@@ -65,7 +126,7 @@ const show = { // Объект содержит методы, добавляющ
 const fire = (event) => {
     const target = event.target; // получаем елемент по которому кликнули
     if(target.classList.length !== 0 || target.tagName !== "TD") return // Если попали между клеток, возвращаем return
-    if (game.shipCount < 1) return; // Если игра окончена возвращаем return чтобы дальше не записывались выстрелы
+    if (!game.shipCount) return; // Если игра окончена возвращаем return чтобы дальше не записывались выстрелы
     show.miss(target); // в объекте show dspsdftv метод miss и передаём в него елемент по которому кликнули
     play.updateData = 'shot'; // в объекте play вызываем метод updateData (сеттер) и передаём в него ключ свойства
    
@@ -88,7 +149,7 @@ const fire = (event) => {
 
                 game.shipCount -= 1;
 
-                if (game.shipCount < 1) {
+                if (!game.shipCount) {
                     header.style.color = 'red';
                     header.textContent = 'Игра окончена!';
 
@@ -107,10 +168,18 @@ const fire = (event) => {
 const init = () => {
     enemy.addEventListener('click',fire);
     play.render();
-    // console.log(location);
+    game.generateShip();
+
     again.addEventListener('click', () => {
         location.reload();
     });
+
+    record.addEventListener('dblclick', () => {
+        play.record = 0;
+        localStorage.removeItem('seaBattleRecord');
+        play.render();
+    });
+    // console.log(game);
 };
 
 init()
